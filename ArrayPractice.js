6c0f25b968d705fb997b45ac0e97e8cb76686190
JJClark6 <jonjclark6@gmail.com>
2024-02-14 16:58:04 -0500

    First Commit

---
+++ ArrayPractice.js
@@ -0,0 +1,16 @@
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+/* Remove Banana from array */
+array.shift();
+
+/* Sort array in order */
+array.sort();
+
+/* Put Kiwi at the end of the array */
+array.push("Kiwi");
+
+/* Remove Apples from array */
+array.shift();
+
+/* Sort in reverse order */
+array.reverse();
